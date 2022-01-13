a55a1a0821e41e8dfe308145d7733953c5b918b9
Jesse B. Hannah <jesse@jbhannah.net>
2022-01-12 23:03:10 -0800

    Serialize sessions as JWT

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,8 +1,65 @@
 # frozen_string_literal: true
 
+require 'base64'
+
 # Session model
 class Session < ApplicationRecord
+  JWT_ALGORITHM = 'ED25519'
+  JWT_DECODE_OPTS = {
+    algorithm: JWT_ALGORITHM,
+    nbf_leeway: 30,
+    verify_iat: true
+  }.freeze
+  JWT_PRIVATE_KEY = RbNaCl::Signatures::Ed25519::SigningKey.new(
+    Base64.decode64(Rails.application.credentials.jwt_private_key_seed)
+  )
+  JWT_PUBLIC_KEY = JWT_PRIVATE_KEY.verify_key
+
+  STRINGIFY_ATTRIBUTES_FOR_SERIALIZATION = %w[created_from last_accessed_from].freeze
+
   belongs_to :user
+
+  before_save :extend_life
+
   scope :active, -> { where(expires_at: Time.zone.now..) }
   scope :expired, -> { inverse_of(:active) }
+
+  class << self
+    def from_jwt(jwt)
+      payload, = JWT.decode(jwt, JWT_PUBLIC_KEY, true, JWT_DECODE_OPTS)
+      active.find_by!(id: payload['jti'], user_id: payload['sub'])
+    end
+  end
+
+  def as_jwt
+    {
+      sub: user_id,
+      exp: expires_at.to_i,
+      nbf: iat,
+      iat: iat,
+      jti: id
+    }
+  end
+
+  def to_jwt
+    JWT.encode(as_jwt, JWT_PRIVATE_KEY, JWT_ALGORITHM)
+  end
+
+  private
+
+  def extend_life
+    self.expires_at = 30.days.from_now
+  end
+
+  def iat
+    @iat ||= created_at.to_i
+  end
+
+  def read_attribute_for_serialization(key)
+    if STRINGIFY_ATTRIBUTES_FOR_SERIALIZATION.include?(key)
+      send(key).to_s
+    else
+      super
+    end
+  end
 end
--- spec/models/session_spec.rb
+++ spec/models/session_spec.rb
@@ -24,5 +24,31 @@ RSpec.describe Session, type: :model do
       it { is_expected.not_to include active_session }
       it { is_expected.to include expired_session }
     end
+
+    describe '.from_jwt' do
+      subject { described_class.from_jwt(jwt) }
+
+      let(:jwt) { session.to_jwt }
+
+      it { is_expected.to have_attributes id: session.id, user_id: session.user_id }
+    end
+  end
+
+  describe '#expires_at' do
+    subject { session.expires_at.to_s }
+
+    before { travel_to session.created_at + 15.days }
+
+    let(:session) { create(:session) }
+    let(:now) { session.created_at + 15.days }
+
+    it { is_expected.to eql (session.created_at + 30.days).to_s }
+    it { is_expected.to eql (now + 15.days).to_s }
+
+    context 'when updated' do
+      before { session.save! }
+
+      it { is_expected.to eql (now + 30.days).to_s }
+    end
   end
 end
