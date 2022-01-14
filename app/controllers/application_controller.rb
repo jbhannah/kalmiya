ba5494b57e1fb66a3621fc1af7f39cb48549b05b
Jesse B. Hannah <jesse@jbhannah.net>
2022-01-13 20:26:18 -0800

    Get current user from JWT in session cookie

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,39 @@
 # frozen_string_literal: true
 
+# Base application controller
 class ApplicationController < ActionController::Base
+  helper_method :current_user, :logged_in?
+
+  after_action :refresh_session, if: -> { logged_in? }
+
+  protected
+
+  def current_session
+    @current_session ||= session_from_jwt
+  end
+
+  def current_user
+    @current_user ||= current_session.user
+  end
+
+  # :reek:NilCheck
+  def logged_in?
+    @logged_in ||= !jwt.nil?
+  end
+
+  private
+
+  def jwt
+    @jwt ||= session[:jwt]
+  end
+
+  def refresh_session
+    session[:jwt] = current_session.to_jwt
+  end
+
+  def session_from_jwt
+    Session.from_jwt(jwt).tap do |jwt_session|
+      jwt_session.update!(last_accessed_from: request.remote_ip, last_accessed_at: Time.zone.now)
+    end
+  end
 end
