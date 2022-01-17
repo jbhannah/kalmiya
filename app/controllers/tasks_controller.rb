faa5fd9d795d948e114f6a79aed449f382e529fb
Jesse B. Hannah <jesse@jbhannah.net>
2022-01-16 21:14:04 -0800

    Create and update tasks over Turbo Streams

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,11 +2,41 @@
 
 # Tasks controller
 class TasksController < ApplicationController
+  before_action :set_task, except: %i[index new create]
+
   def index
     @tasks = current_user.tasks
   end
 
-  def show
+  def new
+    @task = current_user.tasks.new
+  end
+
+  def create
+    task = current_user.tasks.create!(task_params)
+
+    respond_to do |format|
+      format.html { redirect_to task }
+      format.turbo_stream { redirect_to new_task_path }
+    end
+  end
+
+  def show; end
+
+  def edit; end
+
+  def update
+    @task.update!(task_params)
+    redirect_to @task
+  end
+
+  private
+
+  def set_task
     @task = current_user.tasks.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:name)
   end
 end
