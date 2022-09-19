a9385ebf2c29a1be89032f5780acb5c2d08637b0
Jesse Brooklyn Hannah <jesse@jbhannah.net>
2022-09-19 15:35:41 -0700

    Run rubocop -A

--- config/initializers/core_extensions.rb
+++ config/initializers/core_extensions.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
-Dir.glob(Rails.root.join('lib/core_extensions/**/*.rb')).each do |file|
+Rails.root.glob('lib/core_extensions/**/*.rb').each do |file|
   require file
 end
